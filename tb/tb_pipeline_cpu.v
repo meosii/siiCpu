7c4aea97ded4928d311b47a041b2764055d05616
meosii <meosii@meoacs.top>
2023-11-09 19:52:30 +0800

    modify tb

--- tb/tb_pipeline_cpu.v
+++ tb/tb_pipeline_cpu.v
@@ -1,21 +1,27 @@
 `timescale 1ps/1ps
-`include "../rtl/unit/define.v"
+`include "../rtl/define.v"
 module tb_pipeline_cpu ();
 
 reg                         cpu_en;
 reg                         clk;
 reg                         rst_n;
+reg                         irq_external;
+reg                         irq_timer;
+reg                         irq_software;
 wire                        rd_insn_en;
 wire [`PC_WIDTH - 1 : 0]    pc;
 reg [`WORD_WIDTH - 1 : 0]   insn;
 
 pipeline_cpu_top u_pipeline_cpu_top(
-    .cpu_en     (cpu_en     ),
-    .clk        (clk        ),
-    .rst_n      (rst_n      ),
-    .rd_insn_en (rd_insn_en ),
-    .pc         (pc         ),
-    .insn       (insn       )
+    .cpu_en         (cpu_en         ),
+    .clk            (clk            ),
+    .rst_n          (rst_n          ),
+    .irq_external   (irq_external   ),
+    .irq_timer      (irq_timer      ),
+    .irq_software   (irq_software   ),
+    .insn           (insn           ),
+    .rd_insn_en     (rd_insn_en     ),
+    .pc             (pc             )
 );
 
 parameter TIME_CLK = 10;
@@ -30,7 +36,7 @@ end
 
 always @(*) begin
     if (rd_insn_en) begin
-        insn = disk[pc[6:2]];
+        insn = disk[pc[11:2]];
     end else begin
         insn = 0;
     end
@@ -41,6 +47,9 @@ initial begin
         clk = 0;
         cpu_en = 0;
         rst_n = 0;
+        irq_external = 0;
+        irq_timer = 0;
+        irq_software = 0;
     end
     #22 begin
         rst_n = 1;
